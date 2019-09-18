4145469d1411ca2b68d7e278bb82ae7ce585ff54
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-18 17:23:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,12 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  while array.length.times do
+  if value_to_find == array[i]
+    return array.index
+  end
 end
 
 def find_max_value(array)
-  # Add your solution here
+
 end
 
 def find_min_value(array)
