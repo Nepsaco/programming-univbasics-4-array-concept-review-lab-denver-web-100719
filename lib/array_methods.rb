3cc3f2b7912b3cec6ea3318c9a2f46d84987c5c7
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-18 17:33:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,5 @@
 def find_element_index(array, value_to_find)
-  while  do
-  if value_to_find == array[i]
-    return array.index
-  end
+ array.index(value_to_find)
 end
 
 def find_max_value(array)
