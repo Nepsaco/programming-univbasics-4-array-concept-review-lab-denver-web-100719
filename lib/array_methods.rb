856f02f8b6aeeea69d310e0fa7aa1010892cd47b
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-18 18:03:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,10 +8,10 @@ def find_max_value(array)
   while array.length < i do
     if array[i] > max
       max = array[i]
-      i+= 1
     end
-    max
+      i+= 1
   end
+  max
 end
 
 def find_min_value(array)
